a63ba5a6a30c614993359eec2af3ee0e8c94ba63
zhadyra <zhadyra.kurmanova@narxoz.kz>
2026-03-14 11:26:06 +0500

    SingleTargetSkill

--- src/com/narxoz/rpg/bridge/SingleTargetSkill.java
+++ src/com/narxoz/rpg/bridge/SingleTargetSkill.java
@@ -9,8 +9,7 @@ public class SingleTargetSkill extends Skill {
 
     @Override
     public void cast(CombatNode target) {
-        // TODO: Single-target Bridge action
-        // 1) Resolve final damage through effect implementor
-        // 2) Apply to target node
+        int damage = resolvedDamage();
+        target.takeDamage(damage);
     }
 }
